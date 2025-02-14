6c79b1acfaf87f674ca87e38823a0b5a80be0d3b
Alok Singh <aloksingh36757@gmail.com>
2025-02-14 12:57:42 +0530

    till python arrays(list)

--- Day5.py
+++ Day5.py
@@ -62,6 +62,7 @@ if a>b or b>c :
 
 """
 # nested if
+"""
 x =40
 if x>10:
 	print("above 10")
@@ -70,11 +71,201 @@ if x>10:
 	else:
 		print("not above 20")
 
+"""
+#python loops 
+#python have two primitive loop command 
+#1.while loops
+#2.for loops
+"""
+i =1
+while i<=10:
+	print(i)
+	i +=1
+"""
+
+# break statement
+# it will break the loop even the condition is true
+"""
+i =1
+while i<10:
+	print(i)
+	if i == 3:
+		break
+	i +=1
+
+"""
+#continue statement
+"""
+i =0
+while i <6:
+	i +=1
+	if i ==3:
+		continue
+	print(i)
+"""
+
+#Looping through the strings
+"""
+for x in "Banana":
+	print(x)
+"""
+#break statement in for loop
+"""
+fruits  = ["apple","banana","oranges"]
+for x in fruits:
+	print(x)
+	if x == "banana":
+		break
+"""
+#continue statement in for loop
+"""
+fruits  = ["apple","banana","oranges"]
+for x in fruits :
+	if x == "banana":
+		continue
+	print(x)
+"""
+#range() function 
+# range function returns a sequence  of numbers starting form 0 by default and step value is 1
+"""
+for x in range(6):
+	print(x)
+"""
+# we can modify the range function
+
+"""
+for x in range(3,30,3):
+	print(x)
+"""
+"""
+
+for x in range(3,30,3):
+	print(x)
+else:
+	print("FInally finsihed...")
+"""
+# Nested loops
+"""
+adj = ["red","orange","yellow"]
+fruits = ["apple","oranges","magoes"]
+"""
+# for one times of outer loops the inner loops 
+"""
+for x in  adj:
+	for y in fruits:
+			print(x,y)
+"""
+# --------------------------------Python Functions-------------------
+""""
+# function decalarations
+def my_function():
+    print("hello this my function")
+
+# function calling
+my_function()
+"""
+
+# Arguments in a functions
+"""
+
+def my_function(myname):
+    print(myname,"the name as argruments")
+    
+my_function("alex")
+my_function("linux")
+"""
+
+"""
+
+def my_function2(firstname,lastname):
+    print("name of the candidate is ",firstname,lastname)
+
+my_function2("alex","solly")
+"""
+
+# Arbitrary Arguments, *args
+# if we don't know the actual no. of arguments we have to use the *args parameter
+"""
+
+def my_function3(*kids):
+    print("the youngest kids is ",kids[2])
+    
+my_function3("linux","alex","jack","john doe")
+""" 
+
+# Arbitrary keywords Arguments, **args
+# You can also send arguments with the key = value syntax
+
+"""
+
+def my_function4(**kid):
+	print("the last name is",kid["lname"])
+
+my_function4(fname= "alex",lname= "singh")
+"""
 
 
+# default Parameter value
+"""
+
+def my_function5 (mycountry = "India"):
+	print("my country is "+mycountry)
+
+my_function5("Ireland")
+my_function5("Nepal")
+my_function5()
+"""
 
 
+# sending the list as arguments to the functions
 
+"""
 
+def my_function6(fruits):
+	for x in fruits:
+		print(x)
+
+food = ["apple","orange","grapes"]
+my_function6(food)
+"""
+
+# return statements
+
+"""
+
+def my_function7(value):
+    return 5*value
+
+print(my_function7(5))
+"""
+
+# Python Lambda functions
+# A lambda function in Python is a small, anonymous function defined with the lambda keyword instead of the usual def keyword. 
+# It allows you to write quick functions in a single line without formally naming them.
+"""
+square = lambda x: x*x
+print(square(4))
+
+add_lambda = lambda x,y:x+y
+print(add_lambda(4,5))
+
+"""
+#Array in Python
+# creating an array
+car = ["Ford","BMW","Honda","Audi","Ford"]
+x = car[1]
+car[1] = "Range Rover"
+y = car[1]
+print(x)
+print(y)
+print(len(car))
+for x in car:
+    print(x)
+# pop() mehthod
+car.pop(3)
+print(car)
+# remove() method:- it will remove the first occurence of the items
+car.remove("Ford")
+print(car)
 
 
