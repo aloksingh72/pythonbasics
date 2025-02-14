10127ba40edaf59528e9def37c1415e80e1900af
Alok Singh <aloksingh36757@gmail.com>
2025-02-14 10:28:18 +0530

    Day 5 loop started

---
+++ Day5.py
@@ -0,0 +1,80 @@
+# Python if else 
+"""
+a= 23
+b = 67
+if a > b:
+	print("a is the greatest one")
+else:
+	print("b is the greatest one")
+"""
+
+#elif statement
+"""
+a = 72
+b = 72
+if a > b :
+	print("a is the greatest one")
+elif a == b:
+	print(" both are equal")
+"""
+#else statement
+"""
+a = 72
+b = 82
+if a > b :
+	print("a is the greatest one")
+elif a == b:
+	print(" both are equal")
+else:
+	print("b is the greatest one")
+"""
+
+
+#Shorthand if statement
+"""
+a = 34
+b = 86
+print("A") if a > b else print("B")
+"""
+
+#one line if else statement with 3 statement
+"""
+a = 330
+b = 340
+
+print("A") if a > b else print("=") if  a== b else print("B")
+"""
+"""
+a= 200
+b = 89
+c = 78
+if a>b and b > c :
+	 print("A is the greatest")
+ 
+"""
+# OR statement
+"""
+a = 45
+b = 89
+c =78
+if a>b or b>c :
+	print("at least one of the condition is true")
+
+"""
+# nested if
+x =40
+if x>10:
+	print("above 10")
+	if x >20:
+		print("also above 20")
+	else:
+		print("not above 20")
+
+
+
+
+
+
+
+
+
