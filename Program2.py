14f3bdcabb72a3ea5a3343207764d4fe14dad9e7
Alok Singh <aloksingh36757@gmail.com>
2025-02-14 18:02:44 +0530

    Day5 program completed

---
+++ Program2.py
@@ -0,0 +1,42 @@
+# taking the user input
+
+
+# Program 1 to search the number of occurences in the list
+"""
+n = int(input("Enter the value to be search..."))
+numbers = [1,1,1,2,2,3,3,8,8,8,9,9]
+
+occurence = 0
+
+for nums in numbers:
+        if nums == n:
+            occurence +=1
+print(f"the number {n} is found {occurence} times")
+"""
+
+
+numbers = [1,1,2,2,2,3,3,8,8,8,9,9]
+occurence = {
+
+}
+for nums in numbers:
+    if nums not in occurence:
+      occurence[nums] = 1
+    else:
+       occurence[nums] += 1
+
+        
+#    occurence[num = occurence.get(nums,0) + 1
+
+print(occurence)
+
+
+
+
+
+
+
+
+
+
+
