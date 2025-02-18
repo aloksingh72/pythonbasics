90f20fa8ba5f81cc9312c919e994834b938a202f
Alok Singh <aloksingh36757@gmail.com>
2025-02-18 17:39:12 +0530

    Taks 3 completed

--- Task4.py
+++ Task4.py
@@ -1,22 +1,47 @@
 # pattern program
+"""
+
 n =5
 for i in range(1,n+1):
     # for spaces
     for j in range(n-i):
         print(" ",end="")
     # for stars
-    for j in range(2*i-1):
-        print("*",end="")
+    for j in range(i):
+        print("* ",end="")
     print()
+
     # lower part
 for i in range(n-1,0,-1):
     # for spaces
     for j in range(n-i):
         print(" ",end="")
     # for stars
-    for j in range(2*i-1):
-        print("*",end="")
+    for j in range(i):
+        print("* ",end="")
     print()
+"""
+
+n =5
+# upper part
+for i in range(1,n+1):
+    # for spaces
+    for j in range(n-i):
+        print(" ",end="")
+     # for stars
+    for j in range(i):
+        print("* ",end="") 
+    print()
+
+# lower part
+for i in range(n-1,0,-1):
+    # for spaces
+    for j in range(n-i):
+        print(" ",end="")
+    for j in range(i):
+        print("* ",end="")
+    print()
+ 
 
        
     
