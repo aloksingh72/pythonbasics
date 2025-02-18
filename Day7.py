7cdc65029c3b865928bffd89db599f4df8f1565a
Alok Singh <aloksingh36757@gmail.com>
2025-02-18 11:26:14 +0530

    modules completed

--- Day7.py
+++ Day7.py
@@ -12,8 +12,99 @@ thisdict = {
 }
 
 print(len(thisdict))
-"""
+
 
 str1 = "model"
 print(len(str1))
+"""
+# Sccope in Python
+# local scope
+"""
+def myfunc():
+    x = 300
+    def innerfunc():
+        print(x)
+    innerfunc()
+myfunc()
+"""
+# global scope
+"""
+x =200
+def myfunc2():
+    def innerfunc2():
+            x =300
+            print(x)
+    innerfunc2()
+myfunc2()
+print(x)
+"""
+
+
+# Global keywords
+# If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
+
+"""
+def myfunc3():
+    global x 
+    x =300
+    def myfunc4():
+        x =700
+        print(x)
+    myfunc4()
+myfunc3()
+print(x)
+"""
+# to change the global variable  used the keyword global inside the inner function
+
+"""
+x = 400
+def func4():
+    global x
+    x =300
+func4()
+print(x)
+"""
+
+# nonlocal keyword is used to make the variable belong to the outer function
+"""
+def myfunc5():
+    x = "Jane"
+    def innerfunc5():
+        nonlocal x 
+        x = "alex"
+    innerfunc5()
+    return x
+print(myfunc5())
+"""
+
+# Module keywords
+"""
+import platform
+
+x = platform.system()
+print(x)
+"""
+# Note: When using a function from a module, use the syntax: module_name.function_name.
+
+"""
+import module1
+module1.greetingname("Alok")
+"""
+"""
+import module1
+a= module1.person1["age"]
+print(a)
+# if we want to access the mutiple dictionary items
+keys = ["name","age","city"] 
+x = list(map(module1.person1.get,keys))
+print(x)
+"""
+
+# Using the dir() Function
+import platform
+x = dir(platform)
+y = platform.processor()
+z =platform.uname()
+print(y)
+print(z)
 
---
+++ module1.py
@@ -0,0 +1,13 @@
+# creating the module that takes name as argument and returns the greeting string
+# Note: When using a function from a module, use the syntax: module_name.function_name.
+
+
+
+def greetingname(name):
+    print("hello " + name)
+
+person1 = {
+    "name": "John",
+    "age":34,
+    "city":"Noida"
+}
