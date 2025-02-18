7c82c2de3bd58fcc8b0d4e11e15b1dae43f0964d
Alok Singh <aloksingh36757@gmail.com>
2025-02-18 10:32:25 +0530

    Day7 scope started

---
+++ Day7.py
@@ -0,0 +1,19 @@
+# Python Polymorphism
+# The word "polymorphism" means "many forms", and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+
+
+# here len is used as many forms with different data types
+"""
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
+"""
+
+str1 = "model"
+print(len(str1))
+
--- Task3.py
+++ Task3.py
@@ -4,5 +4,5 @@ n=5
 for x in range(1,n+1):
     for x in range(x):
         print("*",end="")
-    print("")    
+    print("")     
 
